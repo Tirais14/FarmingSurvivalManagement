68e00f05dee1469f74af7ebb2bebb8980c176e0f
Tirais14 <100513231+Tirais14@users.noreply.github.com>
2025-07-11 21:08:16 +0300

    PauseMode Update

--- Assets/Main/Scripts/Core/GameModes/PauseMode.cs
+++ Assets/Main/Scripts/Core/GameModes/PauseMode.cs
@@ -5,8 +5,10 @@ namespace Core.GameModes
 {
     public class PauseMode : IGameMode
     {
+        public void Enter() => throw new System.NotImplementedException();
+
         public void Execute() => throw new System.NotImplementedException();
 
-        public void Dispose() => throw new System.NotImplementedException();
+        public void Exit() => throw new System.NotImplementedException();
     }
 }
